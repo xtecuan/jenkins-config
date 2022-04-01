6da454830bde5295d61aee532908874026b56f3b
Tadeo Rivera-Pineda <xtecuan@protonmail.com>
2022-04-01 20:13:40 +0000

    runJenkins.sh changes

--- runJenkins.sh
+++ runJenkins.sh
@@ -2,6 +2,14 @@
 
 export JENKINS_INSTALL=/home/xtecuan/Projects/jenkins-installation
 
+if [ ! -d "$JENKINS_INSTALL" ]
+then
+	echo "Creating $JENKINS_INSTALL"
+	mkdir -p $JENKINS_INSTALL
+else
+	echo "$JENKINS_INSTALL already created!"
+fi
+
 
 docker run -d -u root \
 --privileged --network jenkins --network-alias docker \
